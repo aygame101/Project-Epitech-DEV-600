0a60a06f9ebf5b77b3f64978d4fbe4b444d0dde2
Ryan WITTERT <ryan.wittert@outlook.fr>
2025-03-31 10:49:55 +0200

    cardService changes

--- services/cardService.ts
+++ services/cardService.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import Constants from 'expo-constants';
 
-const API_URL = 'https://api.trello.com/1/';
-const API_KEY = 'your_api_key';
-const TOKEN = 'your_token';
+const API_URL = 'https://api.trello.com/1/'
+const API_KEY = Constants.expoConfig?.extra?.apiKey;
+const TOKEN = Constants.expoConfig?.extra?.token;
+
 
 export const getCards = async (boardId: string, listId: string) => {
   try {
@@ -18,5 +20,54 @@ export const getCards = async (boardId: string, listId: string) => {
     throw error;
   }
 };
+
+// ajouter une nouvelle carte à une liste
+export const addCard = async (listId: string, name: string, desc?: string) => {
+    try {
+      const response = await axios.post(`${API_URL}cards/`, {
+        name,
+        desc,
+        idList: listId,
+        key: API_KEY,
+        token: TOKEN,
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error adding card:', error);
+      throw error;
+    }
+  };
+  
+  // supprimer une carte
+  export const deleteCard = async (cardId: string) => {
+    try {
+      const response = await axios.delete(`${API_URL}cards/${cardId}`, {
+        params: {
+          key: API_KEY,
+          token: TOKEN,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error deleting card:', error);
+      throw error;
+    }
+  };
+  
+  //  mettre à jour une carte
+  export const updateCard = async (cardId: string, name?: string, desc?: string) => {
+    try {
+      const response = await axios.put(`${API_URL}cards/${cardId}`, {
+        name,
+        desc,
+        key: API_KEY,
+        token: TOKEN,
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error updating card:', error);
+      throw error;
+    }
+  };
 
 
